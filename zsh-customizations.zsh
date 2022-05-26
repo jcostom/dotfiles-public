796473033b3dc48beb101d7a845009eb0f32e356
Jason Costomiris <jcostom@jasons.org>
2022-05-26 17:01:20 -0400

    update baseman portainer function

--- zsh-customizations.zsh
+++ zsh-customizations.zsh
@@ -237,6 +237,13 @@ if [ "$_myos" = "Linux" ]; then
             --name=portainer \
             -v /var/run/docker.sock:/var/run/docker.sock \
             -v /var/docks/portainer:/data \
+            -l traefik.enable=true \
+            -l traefik.http.routers.portainer.entrypoints=web,websecure \
+            -l traefik.http.routers.portainer.rule=Host(`portainer.delaware.jcostom.net`) \
+            -l traefik.http.routers.portainer.tls=true \
+            -l traefik.http.routers.portainer.certresolver=letsencrypt \
+            -l traefik.http.services.portainer.loadbalancer.server.port=9443 \
+            -l traefik.http.routers.portainer.middlewares=default-headers@docker \
             portainer/portainer-ce
     }
 
